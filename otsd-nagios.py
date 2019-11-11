91da427397b8a545b1a836f46ea08a18db7278ed
Emanuele Cisbani <emanuele.cisbani@gmail.com>
2019-11-11 12:21:37 +0100

    alpha release good for balance checking
        http errors handling complete
        check balance complete

--- otsd-nagios.py
+++ otsd-nagios.py
@@ -5,9 +5,15 @@ import json
 import sys
 
 # get status from calendar server
+url = sys.argv[1]
 headers = {'Accept': 'application/json', 'user-agent': 'otsd-nagios/0.1.0'}
-url = 'https://otsdev.int4mind.com'
-r = requests.get(url, headers=headers)
+
+try:
+    r = requests.get(url, headers=headers)
+except requests.exceptions.RequestException as error:
+    print("UNKNOWN: http call error = %s" % error)
+    sys.exit(3)
+
 if r.status_code is not 200:
     print("UNKNOWN: http status code = %d" % r.status_code)
     sys.exit(3)
@@ -19,8 +25,22 @@ except:
     print("UNKNOWN: error parsing json response (%s)" % r.text)
     sys.exit(3)
 
-# evaluate status
-print("OK | Balance=%sBTC" % status["balance"])
-
 # dump for debug
 print(json.dumps(r.json(), indent=2))
+
+# evaluate balance
+DUST = 0.001 # This value is too high, and has to be changed server side!
+CRITICAL_BALANCE = DUST
+WARNING_BALANCE = 100*DUST
+
+balance = float(status["balance"])
+if balance < CRITICAL_BALANCE:
+    print("CRITICAL | Balance=%fBTC" % balance)
+    sys.exit(2)
+elif balance < WARNING_BALANCE:
+    print("WARNING | Balance=%fBTC" % balance)
+    sys.exit(1)
+
+print("OK | Balance=%fBTC" % balance)
+
+
