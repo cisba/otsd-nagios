91be52978c6711e115ae703da524f1d8e8a5a63f
Emanuele Cisbani <emanuele.cisbani@gmail.com>
2019-11-12 10:29:48 +0100

    http status codes managed, dust value fixed

--- otsd-nagios.py
+++ otsd-nagios.py
@@ -11,17 +11,28 @@ headers = {'Accept': 'application/json', 'user-agent': 'otsd-nagios/0.1.0'}
 try:
     r = requests.get(url, headers=headers)
 except requests.exceptions.RequestException as error:
+    # request exception is a client error, server status is unknown
     print("UNKNOWN: http call error = %s" % error)
     sys.exit(3)
 
-if r.status_code is not 200:
+if r.status_code >= 500:
+    # http 5xx is a server side error
+    print("CRITICAL: http status code = %d" % r.status_code)
+    sys.exit(2)
+elif r.status_code >= 400:
+    # http 4xx is a client error, server status is unknown
     print("UNKNOWN: http status code = %d" % r.status_code)
     sys.exit(3)
+elif r.status_code is not 200:
+    # http status != 200 is assumed as a server warning
+    print("WARNING: http status code = %d" % r.status_code)
+    sys.exit(1)
 
 # parse json response
 try:
     status = r.json()
 except:
+    # parsing error could be caused by client, server status is unknown
     print("UNKNOWN: error parsing json response (%s)" % r.text)
     sys.exit(3)
 
@@ -29,9 +40,10 @@ except:
 #print(json.dumps(r.json(), indent=2))
 
 # evaluate balance
-DUST = 0.001 # This value is too high, and has to be changed server side!
+# https://github.com/bitcoin/bitcoin/blob/master/src/policy/policy.cpp
+DUST = 0.00000546
 CRITICAL_BALANCE = DUST
-WARNING_BALANCE = 100*DUST
+WARNING_BALANCE = 100 * DUST
 
 balance = float(status["balance"])
 if balance < CRITICAL_BALANCE:
